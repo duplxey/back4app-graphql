c5b9e27babcdbd6ac974cd51d9868c64f714c6cc
Nik Tomazic <nick.tomazic@gmail.com>
2023-03-28 19:21:57 +0200

    chakra configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    heading: "'Open Sans', sans-serif",
+    body: "'Raleway', sans-serif",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
